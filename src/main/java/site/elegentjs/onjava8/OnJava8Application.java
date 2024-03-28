5590794473d746ef393f4f78c53f668fe9d77a9a
liupj <MooN_2012@qq.com>
2024-03-28 14:07:02 +0800

    init

---
+++ src/main/java/site/elegentjs/onjava8/OnJava8Application.java
@@ -0,0 +1,13 @@
+package site.elegentjs.onjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnJava8Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/elegentjs/onjava8/OnJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.elegentjs.onjava8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
